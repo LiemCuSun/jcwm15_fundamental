ff753f483ad0d218e5d6ac8a05aa0bd32df5413a
alee0510 <ali.muksin0510@gmail.com>
2020-11-13 14:43:59 +0700

    update new course

--- index.js
+++ index.js
@@ -80,4 +80,123 @@ Hello("krist")
 // kita deskripsikan user dengan key dan value
 // misal : user punya nama alee => key "nama" dan valuenya "alee"
 
-// CRUD OBJECT => CREATE, READ< UPDATE, AND DELETE
+// CRUD OBJECT => CREATE, READ, UPDATE, AND DELETE
+// CREATE OBJECT
+// 1. literal => secara langsung => {...}
+// 2. constructor => class & functional 
+
+// #LITERAL => {...}
+// case : object user
+// deskripsi : nama, username, alamat, email, password, etc.
+let user1 = {
+    name : "jennie",
+    alamat : "JKT",
+    email : "jenniebabay@gmail.com"
+}
+
+console.log(user1)
+
+// READ value di OBJECT ?
+// array => index
+// object => .key or ["key"]
+console.log(user1.alamat)
+console.log(user1.email)
+console.log(user1["alamat"])
+console.log(user1["email"])
+
+// UPDATE => ADD ATAU EDIT
+// ADD => key/porps : tambahanin deskripsi baru
+user1.sekolah = "Purwadhika"
+console.log(user1)
+user1.rambut = "Pink"
+console.log(user1)
+
+// EDIT => value dari key/props yang sudah ada
+user1.alamat = "Korea"
+user1.rambut = "Black"
+console.log(user1)
+
+// DELETE => key/props => value
+delete user1.rambut
+console.log(user1)
+
+// 2. CLASS => cetakan object => cara untuk bikin object
+// class di pake untuk membentuk suatu object yang sama persis
+// dari segi prop/key
+class User {
+    constructor(name, alamat, email) {
+        this.name = name,
+        this.alamat = alamat,
+        this.email = email
+    }
+}
+
+let user2 = new User("alee", "BSD", "alee@yaoo.com")
+console.log(user2)
+let user3 = new User("lisa", "JKT", "lisabp@gmail.com")
+console.log(user3)
+
+// 3. functional constructor => cetakan object => function untuk bikin object
+function Manusia (nama, usia, hobby) {
+    this.nama = nama
+    this.usia = usia
+    this.hobby = hobby
+}
+
+// kalau misalkan kita bikin function untuk membuat object
+// jangan panggi function secara langsung => Manusia ()
+// tetapi gunakan kata kunci new ... untuk membuat object
+let Manusia1 = new Manusia("serigala", 100, "makan")
+console.log(Manusia1)
+
+// OBJECT YANG MENYIMPAN VALUE => OBJECT, ARRAY, ATAU FUNCTION (METHOD)
+let Mobil = {
+    merk : "Ferarri",
+    warna : "merah",
+    jumlah_roda : 4,
+    automatic : true,
+    jalan : () => {
+        return "mobil berjalan."
+    },
+    fitur : {
+        design : "aerodynamic",
+        harga : "mahal"
+    }
+}
+console.log(Mobil)
+console.log(Mobil.merk)
+console.log(Mobil.jumlah_roda)
+console.log(Mobil.jalan) // console.log fungsinya
+console.log(Mobil.jalan()) // console.log hasil return dari fungsinya
+console.log(Mobil.fitur)
+console.log(Mobil.fitur.design)
+console.log(Mobil.fitur.harga)
+
+// kalau pakai kurung siku
+console.log(Mobil["merk"])
+console.log(Mobil["jalan"])
+console.log(Mobil["jalan"]())
+console.log(Mobil["fitur"]["design"])
+
+// OBJECT DI DALAM ARRAY
+let database = [
+    {
+        nama : "alee",
+        umur : 19,
+        single : true,
+        kenalan : () => `Hello World`
+    },
+    {
+        nama : "jennie",
+        umur : 19,
+        single : true,
+        kenalan : () => `Hello World`
+    }
+]
+console.log(database)
+console.log(database[1])
+console.log(database[1].nama)
+console.log(database[1].umur)
+console.log(database[1]["nama"])
+console.log(database[1]["single"])
+console.log(database[0].kenalan())
